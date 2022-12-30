b7a3956f23278a3d82ccff45dd0539a48d5cc22a
syedkazim110 <97368219+syedkazim110@users.noreply.github.com>
2022-12-30 14:05:23 +0500

    Update admin.py

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from home.models import Order
+from home.models import Order, Customer, Item, Address, Product
 # Register your models here.
 
 admin.site.register(Order)
+admin.site.register(Product)
+admin.site.register(Customer)
+admin.site.register(Item)
+admin.site.register(Address)
